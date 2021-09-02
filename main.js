b9aaaac53d10dff9593e2a745b95d89a33b3b455
Hifi58 <gillet.tony@gmail.com>
2021-09-02 11:31:26 +0200

    code commenté

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ let namein = document.getElementById('namein');
 let income = document.getElementById('income');
 let addincome = document.getElementById('addincome');
 
-// Ajout d'un élément dans le revenu
+
+///////////////////////////////////////
+// Ajout d'un élément dans le revenu //
+///////////////////////////////////////
+
 
 addincome.addEventListener('click', function(e){
     e.preventDefault();
@@ -20,10 +24,18 @@ addincome.addEventListener('click', function(e){
     newincome.id = 'new2';
     newincome.textContent = income.value;
     div.appendChild(newincome);
+
+    ///// Total des revenus /////
+
+    let totalincome = document.getElementById('rtotal');
+    totalincome.textContent = Number(totalincome.textContent) + Number(income);
 });
 
 
-// Ajout d'un élément dans les dépenses
+//////////////////////////////////////////
+// Ajout d'un élément dans les dépenses //
+//////////////////////////////////////////
+
 
 let nameout = document.getElementById('nameout');
 let outcome = document.getElementById('outcome');
@@ -42,4 +54,20 @@ addoutcome.addEventListener('click', function(e){
     newoutcome.id = 'new2';
     newoutcome.textContent = outcome.value;
     div.appendChild(newoutcome);
+    
+    ///// Total des dépenses /////
+
+    let totaloutcome = document.getElementById('dtotal');
+    totaloutcome.textContent -= Number(outcome);
 });
+
+
+
+
+
+
+
+///// Budget total /////
+
+total.textContent = Number(total.textContent) + Number(income);
+total.textContent -= Number(outcome);
